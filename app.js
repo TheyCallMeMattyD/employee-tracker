6775c3ffaaf3741f576454e25f11eadbd530e72a
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-09 18:44:43 -0500

    added password

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: "34rfv#$RFV34rfv",
     database: "employee_trackerDB"
 });
 
