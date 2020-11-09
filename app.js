732d4f8d11231f3c9a01db3c0cf65946f23de7a8
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-09 18:47:57 -0500

    removed password

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "34rfv#$RFV34rfv",
+    password: "password",
     database: "employee_trackerDB"
 });
 
